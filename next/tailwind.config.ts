7e10af34d0b332a56cb26625fd10528991f85b9d
Mitchell Christ <mitchell@human.marketing>
2023-12-15 11:21:45 -0800

    use max-* for targeting downward breakpoint

--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -3,14 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
 	content: ['./src/{app,components}/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
-		extend: {
-			screens: {
-				'<sm': { max: '639px' },
-				'<md': { max: '767px' },
-				'<lg': { max: '1023px' },
-				'<xl': { max: '1279px' },
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 	safelist: ['action'],
