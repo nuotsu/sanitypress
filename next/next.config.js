9ce6c1b1f0660e86dae48d6a63ecce7ae17c8543
Mitchell Christ <mitchell@human.marketing>
2024-01-26 17:12:43 -0800

    update pkg

--- next/next.config.js
+++ next/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['cdn.sanity.io'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sanity.io',
+			},
+		],
 	},
 	// logging: {
 	// 	fetches: {
