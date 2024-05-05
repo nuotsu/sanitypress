e5af9b27ef1ea0a603073e6fc674ab86621dee45
Mitchell Christ <mitchell@human.mareketing>
2024-05-04 17:44:19 -0700

    add "Content dump"

--- sanity/src/structure.ts
+++ sanity/src/structure.ts
@@ -1,7 +1,7 @@
 import { singleton } from './utils'
 import type { StructureResolver } from 'sanity/structure'
 
-import { VscServerProcess } from 'react-icons/vsc'
+import { VscServerProcess, VscSymbolMisc } from 'react-icons/vsc'
 
 const structure: StructureResolver = (S, context) =>
 	S.list()
@@ -17,6 +17,16 @@ const structure: StructureResolver = (S, context) =>
 
 			S.documentTypeListItem('blog.post').title('Blog posts'),
 			S.documentTypeListItem('blog.category').title('Blog categories'),
+			S.divider(),
+
+			S.listItem()
+				.title('Content dump')
+				.icon(VscSymbolMisc)
+				.child(
+					S.documentList()
+						.title('Content dump')
+						.filter(`_type in ['testimonial']`),
+				),
 		])
 
 export default structure
