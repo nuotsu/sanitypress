581818a297570185d4f34ef8c5326167f1ab4aed
Mitchell Christ <xzmaru@gmail.com>
2024-05-26 16:15:51 -0700

    add `.is-external`

--- next/src/ui/header/LinkList.tsx
+++ next/src/ui/header/LinkList.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import InteractiveDetails from './InteractiveDetails'
 import CTA from '@/ui/CTA'
 import { CgChevronRight } from 'react-icons/cg'
@@ -13,7 +14,13 @@ export default function LinkList({ label, links }: Sanity.LinkList) {
 			<ul className="anim-fade-to-b md:frosted-glass left-0 top-full space-y-1 px-3 py-2 max-md:border-l md:absolute md:min-w-max md:rounded md:border md:bg-canvas md:shadow-md">
 				{links?.map((link, key) => (
 					<li key={key}>
-						<CTA className="hover:link" link={link} />
+						<CTA
+							className={cn(
+								'hover:link',
+								link.external?.startsWith('http') && 'is-external',
+							)}
+							link={link}
+						/>
 					</li>
 				))}
 			</ul>
