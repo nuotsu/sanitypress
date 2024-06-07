a5dc1c44281fa709f37f3d8a24e228f0f622fb46
Mitchell Christ <mitchell@human.mareketing>
2024-06-07 16:04:11 -0700

    minor refacorting

--- next/src/ui/Pretitle.tsx
+++ next/src/ui/Pretitle.tsx
@@ -8,7 +8,7 @@ export default function Pretitle({
 	if (!children) return null
 
 	return (
-		<p className={cn('technical text-sm text-neutral-500', className)}>
+		<p className={cn('technical text-neutral-500', className)}>
 			{stegaClean(children)}
 		</p>
 	)
