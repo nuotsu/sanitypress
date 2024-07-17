37ff69103e69cf8467e2d3635bfc8bfbe9c8d560
Mitchell Christ <mitchell@human.mareketing>
2024-07-17 11:26:44 -0700

    add maxDuration

--- src/app/(studio)/admin/[[...index]]/page.tsx
+++ src/app/(studio)/admin/[[...index]]/page.tsx
@@ -3,6 +3,9 @@ import { Studio } from './Studio'
 // Ensures the Studio route is statically generated
 export const dynamic = 'force-static'
 
+// Extend serverless function timeout
+export const maxDuration = 60 // sec
+
 // Set the right `viewport`, `robots` and `referer` meta tags
 export { metadata, viewport } from 'next-sanity/studio'
 
