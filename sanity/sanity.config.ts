3cefaecdd3ea287bb0e0ee480a394d7c372800b5
Mitchell Christ <mitchell@human.mareketing>
2024-08-07 16:58:51 -0700

    add links and start layout

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
 				sanitypressGuideWidget(),
 			],
 		}),
-		visionTool({ title: 'GROQ' }),
+		visionTool(),
 		codeInput(),
 	],
 
--- sanity/src/sanitypressGuideWidget.tsx
+++ sanity/src/sanitypressGuideWidget.tsx
@@ -1,6 +1,9 @@
-import client from '@/lib/sanity/client'
+// import client from '@/lib/sanity/client'
+// import groq from 'groq'
+import { Box, Card, Code, Flex, Heading, Label, Stack, Text } from '@sanity/ui'
+import pkg from '../../package.json'
+import { ArrowTopRightIcon } from '@sanity/icons'
 import type { DashboardWidget, LayoutConfig } from '@sanity/dashboard'
-import groq from 'groq'
 
 export function sanitypressGuideWidget(
 	config: { layout?: LayoutConfig } = {},
@@ -8,16 +11,103 @@ export function sanitypressGuideWidget(
 	return {
 		name: 'Guide',
 		component: Component,
-		layout: config.layout ?? { width: 'auto' },
+		layout: config.layout ?? { width: 'medium' },
 	}
 }
 
+const groups: Array<{
+	title: string
+	links: { label: string; url: string }[]
+}> = [
+	{
+		title: 'Documentation',
+		links: [
+			{ label: 'Documentation', url: 'https://sanitypress.dev/docs' },
+			{
+				label: 'Schema & Modules',
+				url: 'https://sanitypress.dev/docs/schema-and-modules',
+			},
+			{
+				label: 'Modules Playground',
+				url: 'https://sanitypress.dev/playground',
+			},
+			{
+				label: 'Time-based revalidation',
+				url: 'https://sanitypress.dev/docs/time-based-revalidation',
+			},
+		],
+	},
+	{
+		title: 'Guides',
+		links: [
+			{ label: 'The SanityPress Blog', url: 'https://sanitypress.dev/blog' },
+			{
+				label: 'Adding New Modules',
+				url: 'https://sanitypress.dev/blog/adding-new-modules',
+			},
+			{
+				label: 'Adding Jump Links to Modules',
+				url: 'https://sanitypress.dev/blog/adding-jump-links-to-modules',
+			},
+			{
+				label: 'A Guide for Nested Routes',
+				url: 'https://sanitypress.dev/blog/a-guide-for-nested-routes',
+			},
+		],
+	},
+	{
+		title: 'GitHub',
+		links: [
+			{ label: 'README', url: 'https://github.com/nuotsu/sanitypress' },
+			{
+				label: 'Changelog',
+				url: 'https://github.com/nuotsu/sanitypress/releases',
+			},
+			{
+				label: 'Discussions',
+				url: 'https://github.com/nuotsu/sanitypress/discussions',
+			},
+		],
+	},
+]
+
 async function Component() {
-	const site = await client.fetch<Sanity.Site>(groq`*[_type == 'site'][0]`)
+	// const site = await client.fetch<Sanity.Site>(groq`*[_type == 'site'][0]`)
 
 	return (
-		<div>
-			<h2>{site.title || 'SanityPress'} Info</h2>
-		</div>
+		<Card paddingY={4}>
+			<Stack space={4}>
+				<Box paddingX={3} as="header">
+					<Heading size={1} as="h2">
+						<Flex align="center" gap={2}>
+							About SanityPress
+							<Code size={1}>v{pkg.version}</Code>
+						</Flex>
+					</Heading>
+				</Box>
+
+				{groups.map((group, i) => (
+					<Stack space={3} key={i}>
+						<Card borderBottom padding={3}>
+							<Label size={0} muted>
+								{group.title}
+							</Label>
+						</Card>
+
+						{group.links.map((link, key) => (
+							<Stack space={4} paddingX={3} key={key}>
+								<Text size={1}>
+									<a href={link.url} target="_blank">
+										<Flex align="center" gap={2}>
+											{link.label} <ArrowTopRightIcon />
+										</Flex>
+									</a>
+								</Text>
+							</Stack>
+						))}
+					</Stack>
+				))}
+			</Stack>
+		</Card>
 	)
 }
