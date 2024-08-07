1acb912a0c4d17b81eee2b82e3b027405302ae30
Mitchell Christ <mitchell@human.mareketing>
2024-08-07 14:33:06 -0700

    add demo dataset

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,9 +1,8 @@
 import { defineCliConfig } from 'sanity/cli'
-import { projectId, dataset } from './src/env'
 
 export default defineCliConfig({
 	api: {
-		projectId,
-		dataset,
+		projectId: '',
+		dataset: '',
 	},
 })
