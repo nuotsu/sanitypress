24c69608e6eb436096a72b67dc353bf6c3c7fe83
Mitchell Christ <mitchell@human.mareketing>
2024-09-08 16:05:35 -0700

    adjust pagination sticky

--- src/ui/modules/blog/BlogFrontpage/Paginated.tsx
+++ src/ui/modules/blog/BlogFrontpage/Paginated.tsx
@@ -17,7 +17,7 @@ export default function Paginated({ posts }: { posts: Sanity.BlogPost[] }) {
 	useEffect(resetPage, [selected])
 
 	return (
-		<>
+		<div className="relative space-y-12">
 			<List
 				posts={paginatedItems}
 				className="grid gap-x-6 gap-y-12 md:grid-cols-[repeat(auto-fill,minmax(300px,1fr))]"
@@ -27,6 +27,6 @@ export default function Paginated({ posts }: { posts: Sanity.BlogPost[] }) {
 				className="frosted-glass sticky bottom-0 flex items-center justify-center gap-4 bg-canvas p-2 [&_span]:tabular-nums"
 				buttonClassName="hover:underline disabled:opacity-20"
 			/>
-		</>
+		</div>
 	)
 }
