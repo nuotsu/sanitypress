7b86c05056f139b67444bf8aa4864405a365d764
Mitchell Christ <mitchell@human.marketing>
2024-10-30 11:58:49 -0700

    add token placeholder and comment for private Sanity datasets

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ import type { NextConfig } from 'next'
 const client = createClient({
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-	apiVersion: '2024-10-01',
+	// token: process.env.NEXT_PUBLIC_SANITY_TOKEN for private datasets
+	apiVersion: '2024-10-30',
 	useCdn: true,
 })
 
--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -4,7 +4,8 @@ import dev from '@/lib/env'
 export default createClient({
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-	apiVersion: '2024-10-01',
+	// token: process.env.NEXT_PUBLIC_SANITY_TOKEN for private datasets
+	apiVersion: '2024-10-30',
 	useCdn: !dev,
 	stega: {
 		enabled: false,
