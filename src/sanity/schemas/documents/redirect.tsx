d16b7ec1586cadd2a01c85ff94337efa99d9728f
Mitchell Christ <mitchell@human.marketing>
2024-11-14 16:01:46 -0800

    add custom redirect validation

--- src/sanity/schemas/documents/redirect.tsx
+++ src/sanity/schemas/documents/redirect.tsx
@@ -12,14 +12,14 @@ export default defineType({
 			title: 'Redirect from',
 			placeholder: 'e.g. /old-path, /old-blog/:slug',
 			type: 'string',
-			validation: (Rule) => Rule.required(),
+			validation: (Rule) => Rule.custom(validateRedirect),
 		}),
 		defineField({
 			name: 'destination',
 			title: 'Redirect to',
 			placeholder: 'e.g. /new-path, /blog/:slug',
 			type: 'string',
-			validation: (Rule) => Rule.required(),
+			validation: (Rule) => Rule.custom(validateRedirect),
 		}),
 		defineField({
 			name: 'permanent',
@@ -56,3 +56,12 @@ export default defineType({
 		}),
 	},
 })
+
+function validateRedirect(source?: string) {
+	if (!source) return 'Required'
+
+	// starts with either / or http
+	const regex = /^(\/|https?:\/\/)/
+
+	return regex.test(source) ? true : 'Must start with / or http:// or https://'
+}
