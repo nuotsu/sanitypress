040807307d7bcbc371bca28d99d56b9600199e5d
Mitchell Christ <mitchell@human.marketing>
2024-11-25 18:38:54 -0800

    update tabbed-content style

--- src/ui/modules/TabbedContent/TabList.tsx
+++ src/ui/modules/TabbedContent/TabList.tsx
@@ -10,12 +10,14 @@ export default function TabList({
 	const { active, setActive } = tabbedContentStore()
 
 	return (
-		<nav className="max-md:full-bleed flex overflow-x-auto border-b border-accent">
+		<nav className="max-md:full-bleed no-scrollbar flex overflow-x-auto">
 			{tabs?.map((tab, key) => (
 				<button
 					className={cn(
-						'shrink-0 grow basis-[min(150px,80vw)] rounded-t p-2',
-						key === active && 'action rounded-b-none',
+						'shrink-0 grow basis-[min(150px,80vw)] rounded-t border-b p-2 transition-all',
+						key === active
+							? 'border-b-2 border-accent'
+							: 'text-ink/50 hover:text-ink',
 					)}
 					key={key}
 					onClick={() => setActive(key)}
