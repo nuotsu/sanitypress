48bcdc18b2c56809d03b15a48cc7d29d8ec1568c
Mitchell Christ <mitchell@human.marketing>
2024-12-04 10:27:40 -0800

    Revert "add noindex for dev branch"
    This reverts commit 7bbc3cc33eef23651345418a6ed549c983d11f60.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,4 @@
 // import { GoogleTagManager } from '@next/third-parties/google'
-import { dev } from '@/lib/env'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import SkipToContent from '@/ui/SkipToContent'
 import Announcement from '@/ui/Announcement'
@@ -9,10 +8,6 @@ import VisualEditingControls from '@/ui/VisualEditingControls'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import '@/styles/app.css'
-
-export const metadata = {
-	robots: dev ? 'noindex' : undefined,
-}
 
 export default async function RootLayout({
 	children,
