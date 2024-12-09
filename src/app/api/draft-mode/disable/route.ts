b84b8297b50c6bdfb6cf1cb4dd32add94b78c645
Mitchell Christ <mitchell@human.marketing>
2024-12-09 15:45:28 -0800

    minor updates

--- src/app/api/draft-mode/disable/route.ts
+++ src/app/api/draft-mode/disable/route.ts
@@ -3,8 +3,5 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
 	;(await draftMode()).disable()
-
-	const url = new URL(request.nextUrl)
-
-	return NextResponse.redirect(new URL('/', url.origin))
+	return NextResponse.redirect(new URL('/', request.url))
 }
