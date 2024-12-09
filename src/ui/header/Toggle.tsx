fcfd0dc333ae5a4bb8733a1a049f6d666ce8dad9
Mitchell Christ <mitchell@human.marketing>
2024-12-09 12:58:18 -0800

    header-open/closed variant fix

--- src/ui/header/Toggle.tsx
+++ src/ui/header/Toggle.tsx
@@ -1,7 +1,7 @@
 export default function Toggle() {
 	return (
 		<label className="[grid-area:toggle] md:hidden">
-			<input id="header-open" type="checkbox" hidden />
+			<input id="header-toggle" type="checkbox" hidden />
 
 			<span className="header-open:hidden">Open</span>
 			<span className="header-closed:hidden">Close</span>
