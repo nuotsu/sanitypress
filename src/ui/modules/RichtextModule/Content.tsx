259dec4540b58c45c9fc9da02ab4fbc93b839ce0
Mitchell Christ <mitchell@human.marketing>
2025-01-25 13:07:59 -0800

    fix heading spacing in richtext

--- src/ui/modules/RichtextModule/Content.tsx
+++ src/ui/modules/RichtextModule/Content.tsx
@@ -13,7 +13,7 @@ export default function Content({
 	return (
 		<div
 			className={cn(
-				'richtext mx-auto w-full space-y-[1em] [&>:first-child]:!mt-0',
+				'richtext headings:mt-[.5em] mx-auto w-full space-y-4 [&>:first-child]:!mt-0',
 				className,
 			)}
 		>
