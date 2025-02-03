4174d7396d5b7a39c5665fccaf05e788b2ee814e
Mitchell Christ <xzmaru@gmail.com>
2025-02-02 17:07:23 -0800

    add copied state to ClickToCopy component

--- src/ui/ClickToCopy.tsx
+++ src/ui/ClickToCopy.tsx
@@ -1,27 +1,32 @@
 'use client'
 
-import { VscCopy } from 'react-icons/vsc'
+import { VscCheck, VscCopy } from 'react-icons/vsc'
+import { useState, type ComponentProps } from 'react'
 import { cn } from '@/lib/utils'
 
 export default function ClickToCopy({
 	value,
-	children,
 	className,
 	...props
 }: {
 	value?: string
 } & React.ComponentProps<'button'>) {
+	const [copied, setCopied] = useState(false)
 	return (
 		<button
-			className={cn('cursor-copy', className)}
+			className={cn('cursor-copy', copied && 'pointer-events-none', className)}
 			onClick={() => {
 				if (typeof window === 'undefined' || !value) return
+
 				navigator.clipboard.writeText(value)
+
+				setCopied(true)
+				setTimeout(() => setCopied(false), 1000)
 			}}
 			title="Click to copy"
 			{...props}
 		>
-			{children || <VscCopy />}
+			{copied ? <VscCheck /> : <VscCopy />}
 		</button>
 	)
 }
