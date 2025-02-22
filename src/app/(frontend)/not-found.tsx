cceb09b693f2d7df8b94cbc64bd51c54011e5bb6
Mitchell Christ <mitchell@human.marketing>
2025-02-22 14:38:29 -0800

    add global modules to 404 page

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -1,7 +1,7 @@
+import Modules from '@/ui/modules'
 import { fetchSanityLive } from '@/sanity/lib/fetch'
 import { groq } from 'next-sanity'
 import { MODULES_QUERY } from '@/sanity/lib/queries'
-import Modules from '@/ui/modules'
 
 export default async function NotFound() {
 	const page = await get404()
@@ -17,7 +17,14 @@ async function get404() {
 	return await fetchSanityLive<Sanity.Page>({
 		query: groq`*[_type == 'page' && metadata.slug.current == '404'][0]{
 			...,
-			modules[]{ ${MODULES_QUERY} }
+			'modules': (
+				// global modules (before)
+				*[_type == 'global-module' && path == '*'].before[]{ ${MODULES_QUERY} }
+				// page modules
+				+ modules[]{ ${MODULES_QUERY} }
+				// global modules (after)
+				+ *[_type == 'global-module' && path == '*'].after[]{ ${MODULES_QUERY} }
+			)
 		}`,
 	})
 }
