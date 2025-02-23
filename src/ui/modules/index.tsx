eb797cfb862e3031cf594508f754f8852847e214
Mitchell Christ <mitchell@human.marketing>
2025-02-23 14:05:25 -0800

    switch from switch/case to mapping, add dynamic imports

--- src/ui/modules/index.tsx
+++ src/ui/modules/index.tsx
@@ -5,15 +5,12 @@ import BlogPostContent from './blog/PostContent'
 import Breadcrumbs from './Breadcrumbs'
 import Callout from './Callout'
 import CardList from './CardList'
-import CreativeModule from './CreativeModule'
 import CustomHTML from './CustomHTML'
 import FlagList from './FlagList'
 import Hero from './Hero'
 import HeroSplit from './HeroSplit'
 import HeroSaaS from './HeroSaaS'
 import LogoList from './LogoList'
-import PersonList from './PersonList'
-import PricingList from './PricingList'
 import RichtextModule from './RichtextModule'
 import ScheduleModule from './ScheduleModule'
 import SearchModule from './SearchModule'
@@ -22,6 +19,35 @@ import StepList from './StepList'
 import TabbedContent from './TabbedContent'
 import TestimonialList from './TestimonialList'
 import TestimonialFeatured from './TestimonialFeatured'
+
+import dynamic from 'next/dynamic'
+
+const MODULE_MAP = {
+	'accordion-list': AccordionList,
+	'blog-frontpage': BlogFrontpage,
+	'blog-list': BlogList,
+	'blog-post-content': BlogPostContent,
+	breadcrumbs: Breadcrumbs,
+	callout: Callout,
+	'card-list': CardList,
+	'creative-module': dynamic(() => import('./CreativeModule')),
+	'custom-html': CustomHTML,
+	'flag-list': FlagList,
+	hero: Hero,
+	'hero.split': HeroSplit,
+	'hero.saas': HeroSaaS,
+	'logo-list': LogoList,
+	'person-list': dynamic(() => import('./PersonList')),
+	'pricing-list': dynamic(() => import('./PricingList')),
+	'richtext-module': RichtextModule,
+	'schedule-module': ScheduleModule,
+	'search-module': SearchModule,
+	'stat-list': StatList,
+	'step-list': StepList,
+	'tabbed-content': TabbedContent,
+	'testimonial-list': TestimonialList,
+	'testimonial.featured': TestimonialFeatured,
+} as const
 
 export default function Modules({
 	modules,
@@ -32,75 +58,33 @@ export default function Modules({
 	page?: Sanity.Page
 	post?: Sanity.BlogPost
 }) {
+	const getAdditionalProps = (module: Sanity.Module) => {
+		switch (module._type) {
+			case 'blog-post-content':
+				return { post }
+			case 'breadcrumbs':
+				return { currentPage: post || page }
+			default:
+				return {}
+		}
+	}
+
 	return (
 		<>
 			{modules?.map((module) => {
 				if (!module) return null
 
-				switch (module._type) {
-					case 'accordion-list':
-						return <AccordionList {...module} key={module._key} />
-					case 'blog-frontpage':
-						return <BlogFrontpage {...module} key={module._key} />
-					case 'blog-list':
-						return <BlogList {...module} key={module._key} />
-					case 'blog-post-content':
-						return <BlogPostContent {...module} post={post} key={module._key} />
-					case 'breadcrumbs':
-						return (
-							<Breadcrumbs
-								{...module}
-								currentPage={post || page}
-								key={module._key}
-							/>
-						)
-					case 'callout':
-						return <Callout {...module} key={module._key} />
-					case 'card-list':
-						return <CardList {...module} key={module._key} />
-					case 'creative-module':
-						return <CreativeModule {...module} key={module._key} />
-					case 'custom-html':
-						return (
-							<CustomHTML
-								{...(module as Sanity.CustomHTML)}
-								key={module._key}
-							/>
-						)
-					case 'flag-list':
-						return <FlagList {...module} key={module._key} />
-					case 'hero':
-						return <Hero {...module} key={module._key} />
-					case 'hero.split':
-						return <HeroSplit {...module} key={module._key} />
-					case 'hero.saas':
-						return <HeroSaaS {...module} key={module._key} />
-					case 'logo-list':
-						return <LogoList {...module} key={module._key} />
-					case 'person-list':
-						return <PersonList {...module} key={module._key} />
-					case 'pricing-list':
-						return <PricingList {...module} key={module._key} />
-					case 'richtext-module':
-						return <RichtextModule {...module} key={module._key} />
-					case 'schedule-module':
-						return <ScheduleModule {...module} key={module._key} />
-					case 'search-module':
-						return <SearchModule {...module} key={module._key} />
-					case 'stat-list':
-						return <StatList {...module} key={module._key} />
-					case 'step-list':
-						return <StepList {...module} key={module._key} />
-					case 'tabbed-content':
-						return <TabbedContent {...module} key={module._key} />
-					case 'testimonial-list':
-						return <TestimonialList {...module} key={module._key} />
-					case 'testimonial.featured':
-						return <TestimonialFeatured {...module} key={module._key} />
+				const Component = MODULE_MAP[module._type as keyof typeof MODULE_MAP]
 
-					default:
-						return <div data-type={module._type} key={module._key} />
-				}
+				if (!Component) return null
+
+				return (
+					<Component
+						{...module}
+						{...getAdditionalProps(module)}
+						key={module._key}
+					/>
+				)
 			})}
 		</>
 	)
