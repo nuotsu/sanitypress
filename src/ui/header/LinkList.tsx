9b28423a626c7eeb285306c2b22a6f1d761aeb7b
Mitchell Christ <mitchell@human.marketing>
2025-03-26 18:45:26 -0700

    add parentClassName value for common styles on parent level menu links

--- src/ui/header/LinkList.tsx
+++ src/ui/header/LinkList.tsx
@@ -1,8 +1,13 @@
 import InteractiveDetails from './InteractiveDetails'
 import { CgChevronRight } from 'react-icons/cg'
 import CTA from '@/ui/CTA'
+import { cn } from '@/lib/utils'
 
-export default function LinkList({ link, links }: Sanity.LinkList) {
+export default function LinkList({
+	link,
+	links,
+	summaryClassName,
+}: Sanity.LinkList & { summaryClassName?: string }) {
 	return (
 		<InteractiveDetails
 			className="group relative"
@@ -10,7 +15,9 @@ export default function LinkList({ link, links }: Sanity.LinkList) {
 			delay={10}
 			closeAfterNavigate
 		>
-			<summary className="flex h-full items-center gap-1 text-center md:px-3 md:leading-tight">
+			<summary
+				className={cn(summaryClassName, 'flex h-full items-center gap-1')}
+			>
 				{link?.label}
 				<CgChevronRight className="shrink-0 transition-transform group-open:rotate-90 md:rotate-90" />
 			</summary>
--- src/ui/header/Navigation.tsx
+++ src/ui/header/Navigation.tsx
@@ -1,9 +1,12 @@
 import { getSite } from '@/sanity/lib/queries'
 import CTA from '@/ui/CTA'
 import LinkList from './LinkList'
+import { cn } from '@/lib/utils'
 
 export default async function Menu() {
 	const { headerMenu } = await getSite()
+
+	const parentClassName = cn('md:px-3 md:text-center md:leading-tight')
 
 	return (
 		<nav
@@ -15,14 +18,23 @@ export default async function Menu() {
 					case 'link':
 						return (
 							<CTA
-								className="hover:link md:grid md:place-content-center md:px-3 md:text-center md:leading-tight"
+								className={cn(
+									parentClassName,
+									'hover:link md:grid md:place-content-center',
+								)}
 								link={item}
 								key={key}
 							/>
 						)
 
 					case 'link.list':
-						return <LinkList {...item} key={key} />
+						return (
+							<LinkList
+								summaryClassName={parentClassName}
+								{...item}
+								key={key}
+							/>
+						)
 
 					default:
 						return null
