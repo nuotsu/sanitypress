f4e131747f433401f0a10ba2a5465dad7cdf79a8
Mitchell Christ <mitchell@human.marketing>
2025-07-16 10:48:39 -0700

    bump pkg

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 import errors from './errors'
 
 export const dev = process.env.NODE_ENV === 'development'
+
 export const vercelPreview = process.env.VERCEL_ENV === 'preview'
 
 if (!process.env.NEXT_PUBLIC_BASE_URL) {
