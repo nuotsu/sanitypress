3d3d65dbbefa3f41369a3d29a0205e6f2a8b1167
Mitchell Christ <mitchell@human.marketing>
2026-05-21 16:18:59 -0700

    remove temp fix. fresh bun install

--- next.config.ts
+++ next.config.ts
@@ -22,18 +22,6 @@ const nextConfig: NextConfig = {
 		]
 	},
 
-	webpack(config, { isServer }) {
-		if (!isServer) {
-			config.resolve.alias = {
-				...config.resolve.alias,
-				// fix for @sanity/code-input on Vercel prod
-				'@codemirror/state': path.resolve('./node_modules/@codemirror/state'),
-				'@codemirror/view': path.resolve('./node_modules/@codemirror/view'),
-			}
-		}
-		return config
-	},
-
 	async redirects() {
 		return await client.fetch(
 			groq`*[_type == 'redirect']{
