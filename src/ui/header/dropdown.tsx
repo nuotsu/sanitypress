bdedf51362a7d73a8daafbe92239053d35515ac9
Mitchell Christ <mitchell@human.marketing>
2026-06-04 16:50:07 -0700

    minor style adj

--- src/ui/header/dropdown.tsx
+++ src/ui/header/dropdown.tsx
@@ -33,7 +33,7 @@ export default function ({
 			</summary>
 
 			<ul
-				className="md:bg-background anim-fade border-stroke max-md:pl-ch mb-ch md:py-ch top-0 z-1 origin-top leading-tight max-md:border-l md:absolute md:ml-[-1.5ch] md:min-w-max md:border md:p-[1ch_1.5ch] md:shadow-lg starting:-translate-y-1 starting:scale-95"
+				className="md:bg-background anim-fade border-stroke max-md:pl-ch mb-ch md:py-ch top-0 z-1 origin-top leading-tight max-md:border-l md:absolute md:ml-[-1.5ch] md:min-w-max md:border md:p-[1ch_1.5ch] md:shadow-lg starting:-translate-y-1 md:starting:scale-95"
 				style={{
 					positionAnchor: anchorName,
 					positionArea: 'end span-end',
