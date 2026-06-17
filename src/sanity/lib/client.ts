c4b8a921c2e2a192c33de7a2a77364b7d9d2476a
Mitchell Christ <mitchell@human.marketing>
2026-06-17 13:04:29 -0700

    bump pkg

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,14 @@
 import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from '@/sanity/env'
+
+// import { token } from '@/sanity/lib/token'
 
 export const client = createClient({
 	projectId,
 	dataset,
 	apiVersion,
 	useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+	// token, // required for private datasets
 	stega: {
 		studioUrl: '/admin',
 	},
