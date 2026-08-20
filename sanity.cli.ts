c45556579300e71cb66ea519e88809914856e4ff
Mitchell Christ <mitchell@human.marketing>
2026-08-20 14:29:34 -0700

    bump pkg, fix env var usage in sanity CLI file

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,12 +2,16 @@
  * This configuration file lets you run `$ sanity [command]` in this folder
  * Go to https://www.sanity.io/docs/cli to learn more.
  **/
+import { loadEnvConfig } from '@next/env'
 import { defineCliConfig } from 'sanity/cli'
+
+const dev = process.env.NODE_ENV !== 'production'
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
 
 export default defineCliConfig({
 	api: {
-		projectId: '',
-		dataset: 'production',
+		projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+		dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 	},
 	typegen: {
 		enabled: true,
